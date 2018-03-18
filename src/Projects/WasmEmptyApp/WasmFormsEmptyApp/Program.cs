80a169562f4b4f2fd57e6645bdc8b32e0fde13c9
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-03-18 13:53:10 +0100

    Added Wasm (WebAssembly) Xamarin.Forms project template

---
+++ src/Projects/WasmEmptyApp/WasmFormsEmptyApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ooui;
+using Xamarin.Forms;
+
+namespace WasmFormsEmptyApp
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			Forms.Init ();
+
+			UI.Publish ("/", new MainPage ().GetOouiElement ());
+		}
+    }
+}
